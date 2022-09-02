f5fcc62c586f25a3b02c0942883a71de4ac229ce
HaJunRyu <hjr4557@naver.com>
2022-09-02 21:00:18 +0900

    Env: SPA 구현을 위한 react-router-dom을 설정합니다

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { Global } from '@emotion/react';
 import resetCss from '@/styles/reset';
+import { Route, Routes } from 'react-router-dom';
+import Home from '@/pages/Home';
 
 const App = () => (
   <div>
     <Global styles={resetCss} />
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   </div>
 );
 
