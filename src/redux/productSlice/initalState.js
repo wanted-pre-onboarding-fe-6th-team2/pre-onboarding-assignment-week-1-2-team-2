b307f6db659e8c51254efcf9af5295e06b1069ec
DESKTOP-ICDGUQJ\sunghwa <tjdghk3920@naver.com>
2022-09-03 00:10:50 +0900

    Feat: product state 관리를 위한 productSlice 구현

---
+++ src/redux/productSlice/initalState.js
@@ -0,0 +1,23 @@
+export const initialState = [
+  {
+    id: 1,
+    iamgeUrl: 'image',
+    Name: '사탕',
+    option: [
+      {
+        optionId: 1,
+        optionName: '초코맛',
+        optionPrice: 300,
+        optionStock: 10,
+      },
+      {
+        optionId: 2,
+        optionName: '딸기맛',
+        optionPrice: 200,
+        optionStock: 5,
+      },
+    ],
+    price: 100,
+    isDisplay: true,
+  },
+];
