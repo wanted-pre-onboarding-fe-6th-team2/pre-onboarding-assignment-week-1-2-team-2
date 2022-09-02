176563a17bb7eb6003a56ef5ad6e61dfd6100836
DESKTOP-ICDGUQJ\sunghwa <tjdghk3920@naver.com>
2022-09-03 00:11:09 +0900

    Feat: order state 관리를 위한 orderSlice 구현

---
+++ src/redux/orderSlice/initalState.js
@@ -0,0 +1,17 @@
+export const initialState = [
+  {
+    id: 1,
+    option: [
+      {
+        productId: 1,
+        optionId: 1,
+        quantity: 3,
+      },
+      {
+        productId: 1,
+        optionId: 2,
+        quantity: 2,
+      },
+    ],
+  },
+];
