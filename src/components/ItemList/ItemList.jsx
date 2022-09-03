8b49a00ceadbb42a56b21681b9c329d56bd43726
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-09-03 12:22:42 +0900

    Feat: ItemList Component 기본 구조 설정

---
+++ src/components/ItemList/ItemList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as Styled from 'components/ItemList/ItemList.styled';
+
+const ItemList = () => {
+    return (
+        <div>
+          ItemList
+        </div>
+    );
+}
+
+export default ItemList;
---
+++ src/components/ItemList/ItemList.styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const Section = styled.section`
+  width: 100%;
+`;
+
+export { Section };
