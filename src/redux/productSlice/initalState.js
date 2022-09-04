8b3f6f77fa92ce967ef6315849c9fea1a9bcdd33
DESKTOP-ICDGUQJ\sunghwa <tjdghk3920@naver.com>
2022-09-04 21:53:39 +0900

    Revert "Refactor: imageUrl 스펠링 수정"
    This reverts commit 323ff73853283d439d3026e19a4cfeee5e550f62.

--- src/redux/productSlice/initalState.js
+++ src/redux/productSlice/initalState.js
@@ -1,7 +1,7 @@
 export const initialState = [
   {
     id: 1,
-    imgeUrl: 'https://cdn.imweb.me/thumbnail/20200715/53fa50226803f.jpg',
+    iamgeUrl: 'https://cdn.imweb.me/thumbnail/20200715/53fa50226803f.jpg',
     name: '거창 오가네체리자두 3kg 한 상자',
     description:
       '다양한 사탕을 만나보세요!\n 입안에 가득 퍼지는 과일향!\n 한번 맛보면 헤어나올 수 없습니다.',
