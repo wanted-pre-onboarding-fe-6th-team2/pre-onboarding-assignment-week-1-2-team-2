b3bce1dda494415ddd007610d7b27f1f7de8c1f5
nknkcho <srirachacho@gmail.com>
2022-09-05 05:35:43 +0900

    Feat: 상품등록페이지 라우터 연결 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Global } from '@emotion/react';
 import resetCss from '@/styles/reset';
 import { Route, Routes } from 'react-router-dom';
 import Home from '@/pages/Home/Home';
+import ProductRegisterPage from '@/pages/Admin/ProductRegisterPage';
 
 const App = () => (
   <div>
     <Global styles={resetCss} />
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/admin/register" element={<ProductRegisterPage />} />
     </Routes>
   </div>
 );
