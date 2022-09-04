c4b48b00d91382982919d7349d970703496eb43b
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-09-04 19:35:49 +0900

    Feat: `/store` 라우터 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Global } from '@emotion/react';
 import resetCss from '@/styles/reset';
 import { Route, Routes } from 'react-router-dom';
 import Home from '@/pages/Home/Home';
+import ItemList from '@/pages/ItemList/ItemList';
 
 const App = () => (
   <div>
     <Global styles={resetCss} />
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/store" element={<ItemList />} />
     </Routes>
   </div>
 );
