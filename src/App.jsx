a6dc1a51ed1aee2aa4684820fd41e749ebb43286
joohe71 <joohe71@gmail.com>
2022-09-05 00:48:16 +0900

    Feat : 상품상세페이지 경로 연결

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Global } from '@emotion/react';
 import resetCss from '@/styles/reset';
 import { Route, Routes } from 'react-router-dom';
 import Home from '@/pages/Home/Home';
+import ProductDetail from '@/components/ProductDetail/ProductDetail';
 
 const App = () => (
   <div>
     <Global styles={resetCss} />
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/product-detail" element={<ProductDetail />} />
     </Routes>
   </div>
 );
