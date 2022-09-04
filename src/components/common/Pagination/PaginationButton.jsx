197a06582667e0b0900532cc8919cb77fcaf916e
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-09-04 23:36:55 +0900

    Feat: prev, next 버튼 추가 및 처음과 마지막 페이지 alert 추가

--- src/components/common/Pagination/PaginationButton.jsx
+++ src/components/common/Pagination/PaginationButton.jsx
@@ -7,24 +7,45 @@ const PaginationButton = ({ itemsPerPage, totalItemNumber, page, paginate }) => 
   const pageNumbers = Array.from({ length: totalPages }, (_, index) => index + 1);
 
   const handlePageClick = number => {
+    if (number <= 0) {
+      alert('첫번째 페이지입니다.');
+      return;
+    } else if (number > totalPages) {
+      alert('마지막 페이지입니다.');
+      return;
+    }
+
     paginate(number);
   };
 
   // TODO : 10개 이상의 page가 존재하는 경우에 처리하기
   return (
     <Styled.PaginationButtonContainer>
-      {pageNumbers.length <= 10 &&
-        pageNumbers.map((pageNumber, index) => (
-          <Styled.PaginationButton
-            key={index}
-            isCurrentPage={page === pageNumber}
-            onClick={() => {
-              handlePageClick(pageNumber);
-            }}
-          >
-            {pageNumber}
-          </Styled.PaginationButton>
-        ))}
+      <Styled.PaginationButton
+        onClick={() => {
+          handlePageClick(page - 1);
+        }}
+      >
+        prev
+      </Styled.PaginationButton>
+      {pageNumbers.map((pageNumber, index) => (
+        <Styled.PaginationButton
+          key={index}
+          isCurrentPage={page === pageNumber}
+          onClick={() => {
+            handlePageClick(pageNumber);
+          }}
+        >
+          {pageNumber}
+        </Styled.PaginationButton>
+      ))}
+      <Styled.PaginationButton
+        onClick={() => {
+          handlePageClick(page + 1);
+        }}
+      >
+        next
+      </Styled.PaginationButton>
     </Styled.PaginationButtonContainer>
   );
 };
