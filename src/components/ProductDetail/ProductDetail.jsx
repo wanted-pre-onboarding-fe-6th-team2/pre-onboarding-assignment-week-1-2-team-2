523a77da7493dda12f16d20f709d3f7ab22b52e6
joohe71 <joohe71@gmail.com>
2022-09-05 18:25:51 +0900

    Refactor : 상품 상세페이지 동적 구현 위해 useparams 사용

--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -7,12 +7,12 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import { useEffect, useState, useRef } from 'react';
 import { useSelector } from 'react-redux';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet, useNavigate, useParams } from 'react-router-dom';
 import { ROUTES } from '@/constants/route';
 
 const ProductDetail = () => {
-  const params = new URLSearchParams(window.location.search);
-  const productId = params.get('productId');
+  const params = useParams();
+  const productId = params.productId;
   const productData = useSelector(state => state.product).filter(
     data => data.id === Number(productId)
   )[0];
@@ -81,7 +81,7 @@ const ProductDetail = () => {
       alert('상품 옵션을 선택해주세요.');
     } else {
       setIsBuyClicked(prev => !prev);
-      navigate(`${ROUTES.PRODUCTDETAIL}/${ROUTES.ORDER}`);
+      navigate(`/product-detail/${productId}/${ROUTES.ORDER}`);
     }
   };
 
