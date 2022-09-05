1f581115e397aeb0e4a124d9756b77acebc0f3fa
HaJunRyu <hjr4557@naver.com>
2022-09-05 16:20:00 +0900

    Fix: 폴더명을 Mypage -> MyPage로 수정한다

---
+++ src/pages/MyPage/OrderList.jsx
@@ -0,0 +1,12 @@
+import PageContainer from '@/components/common/PageContainer/PageContainer';
+import OrderListForm from '@/components/myPage/OrderList/OrderListForm';
+
+const OrderList = () => {
+  return (
+    <PageContainer as="space">
+      <OrderListForm />
+    </PageContainer>
+  );
+};
+
+export default OrderList;
