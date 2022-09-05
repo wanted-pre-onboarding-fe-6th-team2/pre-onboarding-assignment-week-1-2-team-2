4ba3fe6bdb180f681767d4eeac9b9d647841906d
HaJunRyu <hjr4557@naver.com>
2022-09-05 13:03:56 +0900

    Feat: 더미 데이터 기반 결제 정보 요약 섹션을 구현한다

--- src/components/order/OrderSummary/PaymentSummary.jsx
+++ src/components/order/OrderSummary/PaymentSummary.jsx
@@ -1,29 +1,56 @@
 import * as Styled from '@/components/order/OrderSummary/PaymentSummary.styled';
 
-const PaymentSummaryList = [
-  { title: '주문금액', amount: 200000 },
-  { title: '배송비', amount: 3000 },
-];
+const productListToBuy = {
+  id: 1,
+  name: '거창 오가네체리자두 3kg 한 상자',
+  imageUrl: 'https://cdn.imweb.me/thumbnail/20200715/53fa50226803f.jpg',
+  description: '',
+  discountRate: 15,
+  isDisPlay: true,
+  isLiked: false,
+  shipping: { shippingPrice: '무료 배송', shippingBasis: 3000 },
+  options: [
+    { optionId: 1, optionName: '초코맛 50g', optionPrice: 300, optionStock: 10, quantity: 3 },
+  ],
+};
 
-const PaymentSummary = () => (
-  <Styled.PaymentSummarySection>
-    <Styled.PaymentSummaryHeading>결제 요약</Styled.PaymentSummaryHeading>
-    <Styled.SummaryBox>
-      {PaymentSummaryList.map(({ title, amount }) => (
-        <Styled.SummaryWrapper key={title}>
-          <Styled.SummaryTitle>{title}</Styled.SummaryTitle>
-          <Styled.SummaryContent>{amount.toLocaleString()} 원</Styled.SummaryContent>
+const PaymentSummary = () => {
+  const totalOrderAmount = productListToBuy.options.reduce(
+    (acc, { optionPrice, quantity }) => acc + optionPrice * quantity,
+    0
+  );
+
+  const { shippingPrice } = productListToBuy.shipping;
+
+  const convertedShippingPrice = typeof shippingPrice === 'string' ? 0 : shippingPrice;
+
+  const PaymentSummaryList = [
+    { title: '주문금액', amount: totalOrderAmount },
+    { title: '배송비', amount: shippingPrice },
+  ];
+  return (
+    <Styled.PaymentSummarySection>
+      <Styled.PaymentSummaryHeading>결제 요약</Styled.PaymentSummaryHeading>
+      <Styled.SummaryBox>
+        {PaymentSummaryList.map(({ title, amount }) => (
+          <Styled.SummaryWrapper key={title}>
+            <Styled.SummaryTitle>{title}</Styled.SummaryTitle>
+            <Styled.SummaryContent>
+              {amount.toLocaleString()}
+              {typeof amount !== 'string' && '원'}
+            </Styled.SummaryContent>
+          </Styled.SummaryWrapper>
+        ))}
+        <Styled.DivisionLine />
+        <Styled.SummaryWrapper>
+          <Styled.TotalOrderAmountTitle>최종 결제 금액</Styled.TotalOrderAmountTitle>
+          <Styled.TotalOrderAmountContent>
+            {(totalOrderAmount + convertedShippingPrice).toLocaleString()} 원
+          </Styled.TotalOrderAmountContent>
         </Styled.SummaryWrapper>
-      ))}
-      <Styled.DivisionLine />
-      <Styled.SummaryWrapper>
-        <Styled.TotalOrderAmountTitle>최종 결제 금액</Styled.TotalOrderAmountTitle>
-        <Styled.TotalOrderAmountContent>
-          {(203000).toLocaleString()} 원
-        </Styled.TotalOrderAmountContent>
-      </Styled.SummaryWrapper>
-    </Styled.SummaryBox>
-  </Styled.PaymentSummarySection>
-);
+      </Styled.SummaryBox>
+    </Styled.PaymentSummarySection>
+  );
+};
 
 export default PaymentSummary;
