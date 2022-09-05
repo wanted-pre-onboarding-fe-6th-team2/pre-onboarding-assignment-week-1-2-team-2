289a2294edc87ef614e5325a11916aa5ba119d99
HaJunRyu <hjr4557@naver.com>
2022-09-05 14:43:26 +0900

    Refactor: 상품 등록 페이지를 PageContainer로 감싸준다

--- src/pages/Admin/ProductRegisterPage.jsx
+++ src/pages/Admin/ProductRegisterPage.jsx
@@ -1,6 +1,11 @@
 import ProductRegister from '@/components/Admin/Register';
+import PageContainer from '@/components/common/page/PageContainer';
 
 // TODO: 헤더와 푸터 추가해야 합니다.
-const ProductRegisterPage = () => <ProductRegister />;
+const ProductRegisterPage = () => (
+  <PageContainer>
+    <ProductRegister />
+  </PageContainer>
+);
 
 export default ProductRegisterPage;
